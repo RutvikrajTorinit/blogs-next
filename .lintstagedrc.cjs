edcdcab333ab06de44fdcf4f3c61c7a867ca0f0c
RutvikrajTorinit <rutvikrajsinh.champavat@torinit.ca>
2023-08-01 16:10:43 +0530

    INTG: Shadcn integrated

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     "**/*.(ts|tsx|js)": (filenames) => [
-    //   `npx prettier --write ${filenames.join(" ")}`,
+    `npx prettier --write ${filenames.join(" ")}`,
     `npx eslint ${filenames.join(" ")} --ext ts --ext tsx --ext js --ext jsx`,
     `npx prettier --check ${filenames.join(" ")}`,
     ],
